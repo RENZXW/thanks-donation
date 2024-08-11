81790e83b84066567abe822e152a0b85fd124278
RENZXW <skisbsisdsydsospdsosp@gmail.com>
2024-08-11 11:59:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const lightModeBtn = document.getElementById('light-mode');
 const darkModeBtn = document.getElementById('dark-mode');
 
 // Function to enable light mode
-lightModeBtn.addEventListener('click', () => {
+lightModeBtn.onclick = function() {
     document.body.classList.remove('dark-mode');
-});
+};
 
 // Function to enable dark mode
-darkModeBtn.addEventListener('click', () => {
+darkModeBtn.onclick = function() {
     document.body.classList.add('dark-mode');
-});
+};
